2298b803bf29f782c9b8c21552cc92312efc42cf
League Student <league@iMac-7.attlocal.net>
2019-09-28 12:55:58 -0700

    day two of yeet

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,19 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot reet=new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+		reet.setSpeed(2100000000);
 
 		// 5. Set the pen width to 5
+		reet.penDown();
+		reet.setPenWidth(5);
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for(int i =0; i<4; i++) {
 			// 7. Set the pen color to random
-	
+		reet.setRandomPenColor();
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
+		reet.turn(90);
+		}
 
 	}
 
@@ -26,13 +33,18 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
+		for(int i =0; i <4; i++) {
+		reet.move(100);
+		reet.turn(90);
+		}
 		
 		
 	}
 
 	public static void main(String[] args) {
 		new FourSquare().go();
-	}
+		
+}
 
 }
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,32 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+Robot Grumn=new Robot();
+Grumn.sparkle();
+Grumn.penDown();
+Grumn.setPenColor(250,200,100);
+Grumn.setSpeed(1000);
+Grumn.move(200);
+Grumn.turn(90);
+Grumn.move(10);
+Grumn.turn(90);
+Grumn.move(200);
+Grumn.turn(90);
+Grumn.move(10);
+Grumn.move(202);
+Grumn.turn(92);
+Grumn.move(12);
+Grumn.turn(92);
+Grumn.move(202);
+Grumn.turn(92);
+Grumn.move(12);
+Robot NoU=new Robot();
+NoU.penDown();
+NoU.setPenColor(100,300,10);
+NoU.setSpeed(2000000000);
+for(int i =0; i<200000000; i++) {
+NoU.move(5);
+NoU.turn(1);}
+
 	}
 }
