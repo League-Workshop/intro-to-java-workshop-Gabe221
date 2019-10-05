2d7c481c6385c77a6fe49b66bc01a53a39003736
League Student <league@iMac-9.attlocal.net>
2019-10-05 12:57:26 -0700

    i wrote much yeeeeet
    YEEEEEEEEEEEEEEEEEEEË

---
+++ src/section3/Greeter.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+String DOOT=JOptionPane.showInputDialog("WHUT IS YOUR IKEA NAME");
+JOptionPane.showMessageDialog(null, "Hello " +DOOT);
+}
+
+}
---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello World OWO UWO MUCH YEA BIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIG YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET");
+JOptionPane.showMessageDialog(null,"ERORR PLESE RESTART YOUR COMPUTER");
+JOptionPane.showMessageDialog(null, "NOW!!!!!!!!! OR WE WILL PERSONALY BREAK IN AND YEEEEEEET YOUR VIRUS OUT OF THE SOLAR SYSTEM");
+JOptionPane.showMessageDialog(null, "('')");
+
+}
+}
