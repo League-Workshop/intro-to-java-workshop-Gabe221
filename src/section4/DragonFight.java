98532b8a0e3495bf3cfdb3c0c24c855bb57c4d24
League Student <league@iMac-9.attlocal.net>
2019-10-12 12:58:40 -0700

    REE
    I did much stuffs yeeeeee

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,54 +12,77 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
+		int HP=100;
 	
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
+		Random ran=new Random();
+		int bossHP=500;
 		
 		// 4. Create a variable to hold the damage the player's attack does each round
+		int dmg1=ran.nextInt();
+		int dmg2=ran.nextInt();
 		
 		// 5. Create a variable to hold the damage the dragon's attack does each round
+		int bossdmg=ran.nextInt(51);
 		
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(HP>0 && bossHP>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		int yell=ran.nextInt(101);
+		int kamehameha=ran.nextInt(2000000000);
+			String answer=JOptionPane.showInputDialog("do you want to use yell or kamehameha");
+			
 		// 9. If they typed in "yell":
+			if(answer.equals(yell));{
+				
+		
 		
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
-			//-- Subtract that number from the dragon's health variable 
+			dmg1=ran.nextInt(101);
+			//-- Subtract that number from the dragon's health variable
+			dmg1-=bossHP;
+			}
 			
 		// 10. If they typed in "kick":
+			if(answer.equals(kamehameha)){
 		
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			dmg2=ran.nextInt(2000000001);
+				
 			//-- Subtract that number from the dragon's health variable
+			dmg2-=bossHP;
 			
+			}
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+			bossdmg=ran.nextInt(51);		
 		// 12. Subtract this number from the player's health
+			bossdmg-=HP;
 		
-		
+		}
 		
 		// 13. If the user's health is less than or equal to 0
+		if(HP==0){
 		
 			//-- Tell the user that they lost
+			JOptionPane.showMessageDialog(null, "YOU LOST");
 			
-		
+		}
 		// 14. Else if the dragon's health is less than or equal to 0
+		if(bossHP==0){
 		
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
+			JOptionPane.showMessageDialog(null, "YA RICH NOW");
+		}
 	   //  15.  Else
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
-		}
+	}
 	}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,39 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int score=0;
 		
 		// 2.  Ask the user a question 
+		String answer=JOptionPane.showInputDialog("what is spook");
 		
 		// 3.  Use an if statement to check if their answer is correct
 		
+		if(answer.contentEquals("calcium")){
+		
+		
 		// 4.  if the user's answer was correct, add one to their score 
+			score+=100;
+		}
+		// 5.  Create more questions by repeating steps 1, 2, and 3 below.
+		String answer2=JOptionPane.showInputDialog("why calcium good");
+		if(answer2.contentEquals("because yes")){
+			
+			
+			
+		}
+		if(answer2.contentEquals("yes")) {
+		score+=200;
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		}
 		// 6.  After all the questions have been asked, print the user's score 
+		JOptionPane.showMessageDialog(null,"you're score is "+score);
 		
 	}
 }
+
